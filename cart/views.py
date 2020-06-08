5374e849935d98de63875789b9d5374111223333
danyel66 <danielonyeka247@gmail.com>
2020-06-08 18:47:22 +0100

    integrating redis

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from shop.recommender import Recommender
 
 
 
@@ -33,5 +34,10 @@ def cart_detail(request):
                             'quantity': item['quantity'],
                             'override': True})
     coupon_apply_form = CouponApplyForm()
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    recommended_products = r.suggest_products_for(cart_products,
+                                                  max_results=4)
     return render(request, 'cart/detail.html', {'cart': cart,
-                                                'coupon_apply_form': coupon_apply_form})
+                                                'coupon_apply_form': coupon_apply_form,
+                                                'recommended_products': recommended_products})
